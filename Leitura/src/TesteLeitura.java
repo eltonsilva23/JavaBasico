2eb011390e3545d5946fa7a237479359a2bdc16b
eltonsilva23 <jose.elton@escolar.ifrn.edu.br>
2023-02-13 10:31:48 -0300

    primeiro commit

---
+++ Leitura/src/TesteLeitura.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TesteLeitura {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int valor1;
+		double valor2;
+		System.out.println("Digite um valor:");
+		valor1 = teclado.nextInt();
+		System.out.println("Você digitou o valor");
+	}
+}
